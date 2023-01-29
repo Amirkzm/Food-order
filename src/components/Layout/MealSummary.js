7fdd86779e351b9c40f2801d6b6c64c78fdc6a14
Amir Kazemi <kazemi.amir68@gmail.com>
2023-01-29 18:41:37 +0100

    make MealSummary explanation hidden on mobile

--- src/components/Layout/MealSummary.js
+++ src/components/Layout/MealSummary.js
@@ -6,7 +6,7 @@ const MealSummary = () => {
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
       </p>
-      <p className="hidden lg:block md:inline">
+      <p className="hidden lg:block">
         All our meals are cooked with high-quality ingredients, just-in-time and
         of course by experienced chefs!
       </p>
