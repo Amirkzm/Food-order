1efac3601ba8e5457eec6e5bb35b1b9b264f04cc
Amir Kazemi <kazemi.amir68@gmail.com>
2023-01-29 18:40:12 +0100

    make MealSummary responsive

--- src/components/Layout/MealSummary.js
+++ src/components/Layout/MealSummary.js
@@ -6,7 +6,7 @@ const MealSummary = () => {
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
       </p>
-      <p className="hidden lg:block">
+      <p className="hidden lg:block md:inline">
         All our meals are cooked with high-quality ingredients, just-in-time and
         of course by experienced chefs!
       </p>
